660d5436bea0bc386b88f7237ca01c0b8a143e10
Jurijs Ovsjankins <j.ovsjankins@gmail.com>
2021-12-18 12:22:40 +0200

    test

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -3,6 +3,6 @@ package lv.sda.books;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("test");
     }
 }
