5b0b672ff8c395710cb17f5af82358cfa6e686c1
Jurijs Ovsjankins <j.ovsjankins@gmail.com>
2021-12-18 12:50:17 +0200

    izmainas

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -3,6 +3,6 @@ package lv.sda.books;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Sveiciens");
+        System.out.println("Sveiciens Tev");
     }
 }
