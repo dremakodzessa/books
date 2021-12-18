e604b5615b391eb5e8e95a9278a2b621bea4d463
Jurijs Ovsjankins <j.ovsjankins@gmail.com>
2021-12-18 12:34:17 +0200

    test3 izmainas

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -3,6 +3,6 @@ package lv.sda.books;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("test2");
+        System.out.println("test3"); // jurija izmainas
     }
 }
