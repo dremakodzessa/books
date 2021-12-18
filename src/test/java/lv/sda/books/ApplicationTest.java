38c59bd3df7d649b01cfa33eab0f73920abfd0f5
Undine Berzina <therodcot@gmail.com>
2021-12-18 11:08:11 +0200

    added junit dependency

---
+++ src/test/java/lv/sda/books/ApplicationTest.java
@@ -0,0 +1,12 @@
+package lv.sda.books;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    public void sampleTest(){
+        Assertions.assertEquals(1,1 );
+
+    }
+}
