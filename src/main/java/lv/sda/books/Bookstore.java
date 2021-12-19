aa898d2c05a6bce9f9adae76a6a7e7f27c9ff5b7
Jurijs Ovsjankins <j.ovsjankins@gmail.com>
2021-12-19 11:23:49 +0200

    pedejas

--- src/main/java/lv/sda/books/Bookstore.java
+++ src/main/java/lv/sda/books/Bookstore.java
@@ -1,10 +1,35 @@
 package lv.sda.books;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
 
 public class Bookstore {
+
+
+    //book list
+    private static ArrayList<Book> booksList = new ArrayList<Book>();
+
+
+    public Bookstore() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("C:\\Java\\books\\src\\main\\resources\\BookList.txt"));
+
+            lines.stream().collect(Co)
+        } catch (IOException e) {
+
+        }
+    }
+
+
+
+
+
+
 
     // Add book
     public void addBook(Book book) {
