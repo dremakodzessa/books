42db48c7cb342f1022535a833ee6ef3487802fad
Jurijs Ovsjankins <j.ovsjankins@gmail.com>
2021-12-19 11:36:50 +0200

    valtera palidziba

--- src/main/java/lv/sda/books/Bookstore.java
+++ src/main/java/lv/sda/books/Bookstore.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 
@@ -12,23 +14,29 @@ public class Bookstore {
 
 
     //book list
-    private static ArrayList<Book> booksList = new ArrayList<Book>();
+    private static ArrayList<Book> booksList = new ArrayList<>();
 
 
     public Bookstore() {
         try {
             List<String> lines = Files.readAllLines(Paths.get("C:\\Java\\books\\src\\main\\resources\\BookList.txt"));
-
-            lines.stream().collect(Co)
+            List<Book> books = lines.stream().map(line -> {
+                List<String> fields = Arrays.asList(line.split(";"));
+                return new Book(
+                        fields.get(0),
+                        fields.get(1),
+                        fields.get(2),
+                        fields.get(3),
+                        fields.get(4),
+                        Integer.parseInt(fields.get(5)),
+                        Integer.parseInt(fields.get(6))
+                );
+            }).collect(Collectors.toList());
+            booksList.addAll(books);
         } catch (IOException e) {
 
         }
     }
-
-
-
-
-
 
 
     // Add book
