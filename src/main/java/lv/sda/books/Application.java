14092e8f44be6b855c21299faa1177d4589e5895
Jurijs Ovsjankins <j.ovsjankins@gmail.com>
2022-01-09 11:27:02 +0200

    Clean Code Refactor

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -6,112 +6,44 @@ import java.util.Scanner;
 public class Application {
 
     static Scanner scanner = new Scanner(System.in);
+    static Bookstore bs;
 
     public static void main(String[] args) throws IOException {
-        String input1;
-        String input2;
-        Bookstore bs = new Bookstore();
+        bs = new Bookstore();
 
         while (true) {
-            System.out.println("Menu:");
-            System.out.println("1 - Find book by ISBN");
-            System.out.println("2 - Find book by title");
-            System.out.println("3 - Add book");
-            System.out.println("4 - Remove book");
-            System.out.println("5 - List available books");
-            System.out.println("6 - Save");
-            System.out.println("0 - Quit");
-            System.out.println("======================================");
-            System.out.println("Please enter command number from menu:");
+            printMenu();
 
-            input1 = scanner.nextLine();
+            String input = scanner.nextLine();
 
-            if ("0".equals(input1)) {
+            if ("0".equals(input)) {
                 System.out.println("You have exited the application.");
                 break;
             }
 
-            switch (input1) {
+            switch (input) {
                 case "1":
                     // Search book by ISBN
                     System.out.println("Please enter book ISBN to search:");
-                    input2 = scanner.nextLine();
-                    bs.findByIsbn(input2);
+                    findBook();
                     break;
 
                 case "2":
                     // Search book by title
                     System.out.println("Please enter book title to search:");
-                    input2 = scanner.nextLine();
-                    bs.findByTitle(input2);
+                    findBook();
                     break;
 
                 case "3":
                     // Add book
-                    Book newBook = new Book();
-                    System.out.println("Please enter the following parameters ->");
-
-                    System.out.println("ISBN:");
-                    String i1 = scanner.nextLine();
-                    newBook.setIsbn(i1);
-
-                    System.out.println("Title:");
-                    String i2 = scanner.nextLine();
-                    newBook.setTitle(i2);
-
-                    System.out.println("Author:");
-                    String i3 = scanner.nextLine();
-                    newBook.setAuthor(i3);
-
-                    System.out.println("Publisher:");
-                    String i4 = scanner.nextLine();
-                    newBook.setPublisher(i4);
-
-                    System.out.println("Genre:");
-                    String i5 = scanner.nextLine();
-                    newBook.setGenre(i5);
-
-                    System.out.println("Page count:");
-
-                    while (true){
-
-                        String i6 = scanner.nextLine();
-
-                        try {
-                            newBook.setPages(Integer.parseInt(i6));
-                            break;
-                        }catch (NumberFormatException e){
-                            System.out.println("Wrong input, please try again");
-                        }
-
-                    }
-
-                    System.out.println("Publishing year:");
-
-                    while (true){
-
-                        String i7 = scanner.nextLine();
-
-                        try {
-                            newBook.setPublishingYear(Integer.parseInt(i7));
-                            break;
-                        }catch (NumberFormatException e){
-                            System.out.println("Wrong input, please try again");
-                        }
-
-                    }
-
-
-                    bs.addBook(newBook);
-
+                    addBook();
                     break;
-
 
                 case "4":
                     // Remove book
                     System.out.println("Provide ISBN of the book to remove: ");
-                    input2 = scanner.nextLine();
-                    bs.removeBook(input2);
+                    input = scanner.nextLine();
+                    bs.removeBook(input);
                     break;
 
                 case "5":
@@ -121,7 +53,6 @@ public class Application {
 
                 case "6":
                     // Save to file
-                    bs.deleteFile();
                     bs.saveToFile();
                     System.out.println("All changes saved to the database.");
                     break;
@@ -132,6 +63,68 @@ public class Application {
 
             System.out.println("======================================");
 
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Menu:");
+        System.out.println("1 - Find book by ISBN");
+        System.out.println("2 - Find book by title");
+        System.out.println("3 - Add book");
+        System.out.println("4 - Remove book");
+        System.out.println("5 - List available books");
+        System.out.println("6 - Save");
+        System.out.println("0 - Quit");
+        System.out.println("======================================");
+        System.out.println("Please enter command number from menu:");
+    }
+
+    private static void addBook() {
+        Book newBook = new Book();
+        System.out.println("Please enter the following parameters ->");
+
+        System.out.println("ISBN:");
+        newBook.setIsbn(scanner.nextLine());
+
+        System.out.println("Title:");
+        newBook.setTitle(scanner.nextLine());
+
+        System.out.println("Author:");
+        newBook.setAuthor(scanner.nextLine());
+
+        System.out.println("Publisher:");
+        newBook.setPublisher(scanner.nextLine());
+
+        System.out.println("Genre:");
+        newBook.setGenre(scanner.nextLine());
+
+        System.out.println("Page count:");
+        newBook.setPages(inputNumber());
+
+        System.out.println("Publishing year:");
+        newBook.setPublishingYear(inputNumber());
+
+        bs.addBook(newBook);
+    }
+
+    private static int inputNumber() {
+        while (true) {
+            String number = scanner.nextLine();
+            try {
+                return Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong input, please try again");
+            }
+        }
+    }
+
+    private static void findBook() {
+        String input = scanner.nextLine();
+        Book result = bs.find(input);
+        if (result != null) {
+            System.out.println(result);
+        }else {
+            System.out.println("This book is not found");
         }
     }
 }
